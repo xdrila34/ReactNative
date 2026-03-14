8a9f9f1be67af437b47007e06fbb11ab8766bbe0
Drilon <148321805+xdrila34@users.noreply.github.com>
2026-03-14 11:06:12 +0100

    1
    1

--- react1/App.js
+++ react1/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigation} from '@react-navigation/native';
+import MainScreen from './screens/MainScreen';  
+
+const Stack = createStackNavigation();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={MainScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
