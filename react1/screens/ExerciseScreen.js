da3d01b730516ffc00fe2335cd4e96cd449b67e3
Drilon <148321805+xdrila34@users.noreply.github.com>
2026-03-14 11:45:46 +0100

    1
    moudle3

---
+++ react1/screens/ExerciseScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text, StyleSheet, View} from 'react-native'
+
+const ExersciseScreen = () => {
+    let message = 'hi there';
+    message = 'prsh prej nameku'
+    return(
+        <Text>{message}</Text>
+    )
+}
+
+export default ExersciseScreen;
